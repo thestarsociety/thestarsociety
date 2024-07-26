904be1a1300d1aec6957cb496d9e4f681ab78ed3
Surya <suryakant.subudhi@outlook.com>
2024-07-26 11:32:03 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,12 @@
 "use client";
 
-import React, { useLayoutEffect, useState } from "react";
-import { gsap } from "gsap";
-import Loader from "@/components/custom/loader";
-import Hero  from "@/components/custom/hero";
+import React from "react";
 
 const Home: React.FC = () => {
-  const [loaderFinished, setLoaderFinished] = useState<boolean>(false);
-  const [timeline, setTimeline] = useState<gsap.core.Timeline | null>(null);
-
-  useLayoutEffect(() => {
-    const context = gsap.context(() => {
-      const tl = gsap.timeline({
-        onComplete: () => setLoaderFinished(true),
-      });
-      setTimeline(tl);
-    });
-
-    return () => context.revert();
-  }, []);
 
   return (
     <main>
-      {loaderFinished ? <Hero /> : <Loader timeline={timeline} />}
+      hii
     </main>
   );
 };
